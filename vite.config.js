2a5da614d68b5f8b0a79f0c05b4eab6affd085f3
limping-robot <stephanw430@gmail.com>
2026-01-04 03:09:19 +0100

    ignore dist/

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/auth": {
+      "/login": {
         target: "http://localhost:5000",
         changeOrigin: true,
       },
